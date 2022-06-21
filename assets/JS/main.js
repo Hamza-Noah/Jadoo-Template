200f5d9f121fe9ea224dc13ddd8c5ac2d6e12a34
unknown <hamza.nouh97@gmail.com>
2022-06-21 12:20:08 +0200

    added the destination section

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,21 +1,45 @@
-$('.category-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false,
-            dots: false
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:4,
-            nav:true,
-            loop:false
-        }
-    }
-})
+$(".category-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+      dots: false,
+    },
+    600: {
+      items: 1,
+      nav: false,
+      dots: false,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+      loop: false,
+    },
+  },
+});
+
+$(".destination-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+      dots: false,
+    },
+    600: {
+      items: 1,
+      nav: false,
+      dots: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
