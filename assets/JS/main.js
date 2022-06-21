a34aaa1a27fe4a9a0af2ec10aba8865d4b0bd82f
unknown <hamza.nouh97@gmail.com>
2022-06-21 10:02:32 +0200

    added the category section

---
+++ assets/JS/main.js
@@ -0,0 +1,21 @@
+$('.category-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            dots: false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
