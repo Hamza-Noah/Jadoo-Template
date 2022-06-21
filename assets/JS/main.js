191832cfe6d30cbf30917f07a377a319c1aa289c
unknown <hamza.nouh97@gmail.com>
2022-06-21 17:03:59 +0200

    added the form and trip sections

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -43,3 +43,27 @@ $(".destination-carousel").owlCarousel({
     },
   },
 });
+
+
+$("#brands-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+      dots: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+      dots: false,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false,
+    },
+  },
+});
