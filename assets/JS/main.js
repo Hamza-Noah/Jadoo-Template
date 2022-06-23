3d3bda72533113d71fdfa3b3af83fb42bb295095
unknown <hamza.nouh97@gmail.com>
2022-06-23 11:30:17 +0200

    added the animation to testimonials

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,74 +1,89 @@
-$(".category-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false,
-      dots: false,
+$(document).ready(function () {
+  $(".category-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false,
+      },
+      768: {
+        items: 2,
+        nav: false,
+        dots: false,
+      },
+      1000: {
+        items: 4,
+        nav: false,
+        loop: false,
+        dots: false,
+      },
     },
-    768: {
-      items: 2,
-      nav: false,
-      dots: false,
+  });
+
+  $(".destination-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false,
+      },
+      600: {
+        items: 1,
+        nav: false,
+        dots: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
     },
-    1000: {
-      items: 4,
-      nav: false,
-      loop: false,
-      dots: false,
+  });
+
+  $("#brands-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false,
+      },
+      567: {
+        items: 2,
+        nav: false,
+        dots: false,
+      },
+      767: {
+        items: 3,
+        nav: false,
+        dots: false,
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false,
+      },
     },
-  },
+  });
 });
 
-$(".destination-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false,
-      dots: false,
-    },
-    600: {
-      items: 1,
-      nav: false,
-      dots: false,
-    },
-    1000: {
-      items: 3,
-      nav: true,
-      loop: false,
-    },
-  },
+let firstTestimonial = document.querySelector(".testimonial:first-child");
+let secondTestimonial = document.querySelector(".testimonial:last-child");
+
+firstTestimonial.addEventListener("click", function () {
+  firstTestimonial.classList.remove("hidden-top");
+  secondTestimonial.classList.add("hidden-bottom");
 });
 
-$("#brands-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false,
-      dots: false,
-    },
-    567: {
-      items: 2,
-      nav: false,
-      dots: false,
-    },
-    767: {
-      items: 3,
-      nav: false,
-      dots: false,
-    },
-    1000: {
-      items: 5,
-      nav: true,
-      loop: false,
-    },
-  },
+secondTestimonial.addEventListener("click", function () {
+  secondTestimonial.classList.remove("hidden-bottom");
+  firstTestimonial.classList.add("hidden-top");
 });
