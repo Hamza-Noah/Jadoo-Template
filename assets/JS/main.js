ea3d90c5001b4613d8c4454d3f368bb5ff1e8363
unknown <hamza.nouh97@gmail.com>
2022-06-23 16:07:41 +0200

    added some notes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -82,11 +82,46 @@ let progress = document.getElementById("progress");
 bar.style.cssText =
   " height: 10px; width: 150px; border-radius: 5px; margin-top: 15px; background-color: #F5F5F5;";
 
-  progress.style.cssText = `height: 100%; width: ${barValue}%; border-radius: 5px; background-color: #8A79DF `
-
+progress.style.cssText = `height: 100%; width: ${barValue}%; border-radius: 5px; background-color: #8A79DF `;
 
 let firstTestimonial = document.querySelector(".testimonial:first-child");
 let secondTestimonial = document.querySelector(".testimonial:last-child");
+let btnPrevious = document.getElementById("next-testimonial");
+let btnNext = document.getElementById("previous-testimonial");
+let dotNext = document.getElementById("next-test");
+let dotPrev = document.getElementById("prev-test");
+
+btnNext.addEventListener("click", function () {
+  firstTestimonial.classList.add("hidden-top");
+  secondTestimonial.classList.remove("hidden-bottom");
+  btnNext.setAttribute("disabled");
+  btnPrevious.removeAttribute("disabled");
+});
+
+btnPrevious.addEventListener("click", function () {
+  firstTestimonial.classList.remove("hidden-top");
+  secondTestimonial.classList.add("hidden-bottom");
+  btnPrevious.setAttribute("disabled");
+  btnNext.removeAttribute("disabled");
+});
+
+dotNext.addEventListener("click", function () {
+  firstTestimonial.classList.add("hidden-top");
+  secondTestimonial.classList.remove("hidden-bottom");
+  dotNext.removeAttribute("disabled");
+  dotPrev.setAttribute("disabled");
+  dotNext.classList.add("active");
+  dotPrev.classList.remove("active")
+});
+
+dotPrev.addEventListener("click", function () {
+  firstTestimonial.classList.remove("hidden-top");
+  secondTestimonial.classList.add("hidden-bottom");
+  dotNext.setAttribute("disabled");
+  dotPrev.removeAttribute("disabled");
+  dotPrev.classList.add("active");
+  dotNext.classList.remove("active")
+});
 
 firstTestimonial.addEventListener("click", function () {
   firstTestimonial.classList.remove("hidden-top");
